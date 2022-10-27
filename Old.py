7d2e3189c4d2e00b3c2c8246ce102237fbc53143
Jhalla33 <116783024+Jhalla33@users.noreply.github.com>
2022-10-27 14:34:55 +0500

    Update Old.py
    git clone https://github.com/Jhalla33/Old.git
    ls
    cd Old
    ls
    python Old.py

--- Old.py
+++ Old.py
@@ -53,11 +53,7 @@ br.addheaders = [('Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Fire
 br.addheaders = [('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36')]
 
 def clear():
-	os.system('clear')
-
-
-	os.sys.exit()
-
+	
 def acak(b):
     w = 'ahtdzjc'
     d = ''
